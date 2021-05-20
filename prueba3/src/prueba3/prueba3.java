0a24d0e98b0d131685eb4f82c373cb0174b057de
dw1a <dw1a@DW1A-11>
2021-05-20 20:22:25 +0200

    segundo

--- prueba3/src/prueba3/prueba3.java
+++ prueba3/src/prueba3/prueba3.java
@@ -5,6 +5,7 @@ public class prueba3 {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("primer");
+		System.out.println("segundo");
 	}
 
 }
