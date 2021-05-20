c4ad70b0541959ba435561569ab644b61947e684
dw1a <dw1a@DW1A-11>
2021-05-20 20:21:16 +0200

    primer

---
+++ prueba3/src/prueba3/prueba3.java
@@ -0,0 +1,10 @@
+package prueba3;
+
+public class prueba3 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("primer");
+	}
+
+}
